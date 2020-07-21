6ef60c86de8dfcb0b09922a1a78b68b174f1d843
Robin Langlois <langlois.robin@gmail.com>
2020-07-21 16:26:02 +0200

    commit complete MORE JOIN exercises

--- more_join_operations.sql
+++ more_join_operations.sql
@@ -54,3 +54,81 @@ ON movie.id = casting.movieid
 JOIN actor
 ON casting.actorid = actor.id
 WHERE name = 'Harrison Ford'
+
+#Question 9 - Harrison Ford as a supporting actor
+SELECT title
+FROM movie
+JOIN casting
+ON movie.id = casting.movieid
+JOIN actor
+ON casting.actorid = actor.id
+WHERE name = 'Harrison Ford'
+AND ord!=1
+
+#Question 10 - Lead actors in 1962 movies
+SELECT title, name
+FROM movie
+JOIN casting
+ON movie.id = casting.movieid
+JOIN actor
+ON casting.actorid = actor.id
+WHERE yr = 1962
+AND ord = 1
+
+#Question 11 - Busy years for Rock Hudson
+SELECT yr, COUNT(*) AS busy
+FROM movie
+JOIN casting
+ON movie.id = casting.movieid
+JOIN actor
+ON casting.actorid = actor.id
+WHERE name = 'Rock Hudson'
+GROUP BY yr
+HAVING busy > 2
+
+#Question 12 - Lead actor in Julie Andrews movies
+SELECT title, name
+FROM movie
+JOIN casting
+ON
+movie.id = casting.movieid AND ord=1
+JOIN actor
+ON casting.actorid = actor.id
+WHERE movie.id IN (
+	SELECT id
+	FROM movie
+	JOIN casting
+	ON
+	movie.id = casting.movieid
+	WHERE actorid IN (
+	  SELECT id FROM actor
+	  WHERE name='Julie Andrews')
+	)
+
+#Question 13 - Actors with 15 leading roles
+SELECT name
+FROM actor
+JOIN casting
+ON actor.id = casting.actorid AND ord=1
+GROUP BY name
+HAVING COUNT(*) >= 15
+ORDER BY name ASC
+
+#Question 14 - List the films released in the year 1978 ordered by the number of actors in the cast, then by title.
+SELECT title, COUNT(actorid) AS cast_am
+FROM movie
+JOIN casting ON movie.id = casting.movieid
+WHERE yr = 1978
+GROUP BY title
+ORDER by cast_am DESC, title 
+
+#Question 15 - List all the people who have worked with 'Art Garfunkel'.
+SELECT actor.name
+FROM casting 
+JOIN actor ON actorid=actor.id
+JOIN
+  (SELECT movieid, name FROM casting JOIN actor
+   ON actor.id=actorid WHERE
+   name='Art Garfunkel') AS garf
+ON garf.movieid=casting.movieid
+WHERE actor.name<>garf.name
